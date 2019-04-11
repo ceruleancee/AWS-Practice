84d3dafca406fcf4b17f8cfb66deb5ff18598f66
Claudia Ortiz <claudia@graylog.com>
2019-04-11 12:21:48 -0500

    Add Credentials file

---
+++ src/Credentials.java
@@ -0,0 +1,10 @@
+/*
+ * Author: CeruleanCee
+ * Created: 04/11/2019
+ */
+
+
+public class Credentials {
+    public static final String accessKeyId ="keeganMichaelKey";
+    public static final String secretAccessKey = "keeganMichaelKey";
+}
