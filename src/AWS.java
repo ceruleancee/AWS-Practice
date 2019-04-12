54c21cba840326fab7d26ea30a8ad1619d2cf22d
Claudia Ortiz <claudia@graylog.com>
2019-04-12 16:21:34 -0500

    Add AWS class

---
+++ src/AWS.java
@@ -0,0 +1,24 @@
+/*
+ * Author: CeruleanCee
+ * Created: 04/11/2019
+ */
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.kinesis.KinesisAsyncClient;
+
+public class AWS {
+
+    // Select a region and create a client
+    Ec2Client ec2 = Ec2Client.builder()
+            .region(Region.US_EAST_1)
+            //.endpointOverride(URI.create("https://ec2.us-west-1.amazonaws.com"))
+            .build();
+
+
+    // Create an HTTP connection
+    KinesisAsyncClient client = KinesisAsyncClient.builder()
+            .build();
+
+}
+
