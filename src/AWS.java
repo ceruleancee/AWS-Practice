89c9ae6a5191ecd2c31f5cd5ac1eb6826ab6564b
Claudia Ortiz <claudia@graylog.com>
2019-04-23 11:11:24 -0500

    Delete AmazonDynamoDB object

--- src/AWS.java
+++ src/AWS.java
@@ -4,8 +4,8 @@
  */
 
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -13,21 +13,21 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 public class AWS {
 
     // Set default credentials
-    DefaultAWSCredentialsProviderChain chain = new DefaultAWSCredentialsProviderChain();
-    AWSCredentials cred = chain.getCredentials();
+    BasicAWSCredentials awsCredentials = new BasicAWSCredentials(Credentials.accessKeyId, Credentials.secretAccessKey);
 
     // Set region
     Regions region = Regions.US_EAST_1;
-    //ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider("myProfile");
 
-    // Create Amazon DynamoDB client builder
+    // Check role permissions
+
+    // Pull log
+
+
+    ClientConfiguration configuration = new ClientConfiguration();
+
+
+    // Create a NoSQL database
     AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClientBuilder.standard();
-
-    AmazonDynamoDB dynamoDB =
-            dynamoDB = AmazonDynamoDBClientBuilder.standard()
-                    .withRegion(region)
-                    .withCredentials(chain)
-                    .build();
 
 }
 
