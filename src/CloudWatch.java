0a17d8a5e93648cb13ba1760dc5a0bb6ee155660
Claudia Ortiz <claudia@graylog.com>
2019-04-25 16:38:50 -0500

    Update CloudWatch class

--- src/CloudWatch.java
+++ src/CloudWatch.java
@@ -3,19 +3,12 @@
  * Created: 04/23/2019
  */
 
-import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
-import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
-import com.amazonaws.services.cloudwatch.model.ListMetricsRequest;
-import com.amazonaws.services.cloudwatch.model.ListMetricsResult;
-import com.amazonaws.services.cloudwatch.model.Metric;
+import com.amazonaws.services.logs.AWSLogs;
+import com.amazonaws.services.logs.AWSLogsClientBuilder;
 
 public class CloudWatch {
 
-    AmazonCloudwatch cw = AmazonCloudWatchClientBuilder.defaultClient();
-
-    ListMetricsRequest request = new ListMetricRequest()
-            .withMetricName(name)
-            .withNamespace(namespace);
+    AWSLogs awslogs = AWSLogsClientBuilder.defaultClient();
 
 
 }
