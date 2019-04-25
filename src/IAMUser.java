a3b01037f2d52297a55d9569fc40fbd9144cba32
Claudia Ortiz <claudia@graylog.com>
2019-04-25 16:39:49 -0500

    Rename IAM to IAMUser

---
+++ src/IAMUser.java
@@ -0,0 +1,11 @@
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
+
+public class IAMUser {
+
+    AmazonIdentityManagement iam = AmazonIdentityManagementClientBuilder.defaultClient();
+
+    // Get user
+
+
+}
