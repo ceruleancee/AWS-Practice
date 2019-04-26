178119e329fa65c12c92d4f15c2fab88c4965673
Claudia Ortiz <claudia@graylog.com>
2019-04-26 16:22:54 -0500

    Rename CloudWatch class to CloudWatchPolling, and update class

---
+++ src/CloudWatchPolling.java
@@ -0,0 +1,41 @@
+/*
+ * Author: CeruleanCee
+ * Created: 04/23/2019
+ */
+
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
+import software.amazon.awssdk.services.cloudwatchlogs.model.GetLogEventsRequest;
+
+public class CloudWatchPolling {
+
+    public CloudWatchLogsClient logsClient;
+
+    // Create a CloudwatchLog client
+    public CloudWatchLogsClient createCloudWatchLogClient(AwsCredentials basicCredentials, Region region) {
+
+        this.logsClient = CloudWatchLogsClient.builder()
+                // TODO resolve credentialProvider value with static values
+                .credentialsProvider(StaticCredentialsProvider.create(basicCredentials))
+                .region(region)
+                .build();
+
+        return logsClient;
+    }
+
+    public CloudWatchLogsClient pullLog(String logGroupName, String logStreamName){
+        this.logsClient.getLogEvents(GetLogEventsRequest.builder()
+                                                        .logGroupName(logGroupName)
+                                                        .logStreamName(logStreamName)
+                                                        .startFromHead(true)
+                                                        .build());
+        // TODO not sure about this return
+        return logsClient;
+
+    }
+
+
+
+}
