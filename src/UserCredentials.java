e305def07f7bef4ba17f00e5ad1e77b9e6275711
Claudia Ortiz <claudia@graylog.com>
2019-04-26 16:19:25 -0500

    Rename Credentials to UserCredentials

---
+++ src/UserCredentials.java
@@ -0,0 +1,11 @@
+/*
+ * Author: CeruleanCee
+ * Created: 04/11/2019
+ */
+
+
+public class UserCredentials {
+    public static final String accessKey ="Key";
+    public static final String secretKey = "Key";
+
+}
